b76e47873869b42d1b0f6b6183b3d89ef8a96a96
SianTharit <Tharit.preecha1@gmail.com>
2022-09-08 15:39:46 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "shards-ui/dist/css/shards.min.css"
+import Chat from "./Chat";
+
+function App() {
+  return (
+    <div className="App">
+      <Chat />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Chat.jsx
@@ -0,0 +1,133 @@
+import React, { useState } from 'react'
+import { ApolloClient, InMemoryCache, ApolloProvider, useSubscription, gql, useMutation } from '@apollo/client';
+import { WebSocketLink } from "@apollo/client/link/ws"
+import { Container, Row, Col, FormInput, Button } from "shards-react"
+
+
+const link = new WebSocketLink({
+    uri: `ws://localhost:4000/`,
+    options: {
+        reconnect: true
+    }
+  });
+  
+
+const client = new ApolloClient({
+    link,
+    uri: 'http://localhost:4000/',
+    cache: new InMemoryCache(),
+});
+
+const GET_MESSAGES = gql`
+    subscription {
+        messages {
+            id
+            content
+            user
+    }
+  }
+`;
+
+const POST_MESSAGE = gql`
+  mutation ($user:String!, $content:String!) {
+    postMessage(user: $user, content: $content)
+  }
+`;
+
+const Messages = ({ user }) => {
+    const { data } = useSubscription(GET_MESSAGES);
+    if (!data) {
+        return null;
+    }
+
+    return (
+        <div>
+            { data.messages.map(({id, user: messageUser, content}) => (
+                    <div style={{display: 'flex',justifyContent: user === messageUser ? 'flex-end' : 'flex-start'}}>
+                        <div style={{display: 'flex', flexDirection: "column"}}>  
+                        {user !== messageUser && (
+                            <span style={{
+                                width: 50,
+                                display: 'flex'
+                            }}>
+                                <span>คุณ</span>
+                                { messageUser.toUpperCase() }
+                            </span>
+                        )}
+                        <div style={{
+                            background: user === messageUser ? "#58bf56": "#e5e6ea",
+                            color: user === messageUser ? "white": "black",
+                            padding: "1em",
+                            borderRadius: "1em",
+                        }}>
+                            { content }
+                        </div>
+                        </div>
+                    </div>
+            ))}
+        </div>
+    )
+}
+
+const Chat = () => {
+    const [state, setState] = useState({
+        user: "Sian",
+        content: ""
+    });
+
+     const [ postMessage ] = useMutation(POST_MESSAGE) 
+
+    const onSend = () => {
+        if (state.content.length > 0) {
+            postMessage({
+                variables: state,
+            })
+        }
+        setState({
+            ...state,
+            content: ""
+        })
+    }
+
+    return (
+        <Container>
+            <Messages user={state.user} />
+            <Row>
+                <Col xs={2} style={{ padding: 0 }}>
+                    <FormInput 
+                        lable="User"
+                        value={state.user}
+                        onChange={(event) => setState({
+                            ...state, user: event.target.value,
+                        })}
+                    />
+                </Col>
+                <Col xs={8}> 
+                    <FormInput 
+                        lable="Content"
+                        value={state.content}
+                        onChange={(event) => setState({
+                            ...state, content: event.target.value,
+                        })}
+                        onKeyUp = {(event) => {
+                            if (event.keyCode === 13) {
+                                onSend()
+                            }
+                        }}
+                    />
+                </Col>
+                <Col xs={2}>
+                    <Button onClick={() => onSend()}>
+                        send
+                    </Button>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default () => (
+    <ApolloProvider client={client}>
+        <Chat />
+    </ApolloProvider>
+)
